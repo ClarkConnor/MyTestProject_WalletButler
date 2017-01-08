d5226a041fca6766bf56848b05c7291b0a33a7f8
clarkconnor <clark@CLARK-PC>
2017-01-08 18:57:11 +0800

    Add project files.

---
+++ WalletButler/UI/AddPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WalletButler
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AddPage : Page
+    {
+        public AddPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void homePage_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+
+        private void expend_Switch(object sender, RoutedEventArgs e)
+        {
+            expend.IsChecked = true;
+            if (expend.IsChecked == true)
+            {
+                income.IsChecked = false;
+            }
+            
+        }
+
+        private void income_Switch(object sender, RoutedEventArgs e)
+        {
+            income.IsChecked = true;
+            
+            if (income.IsChecked == true)
+            {
+                expend.IsChecked = false;
+            }
+
+        }
+
+
+    }
+}
